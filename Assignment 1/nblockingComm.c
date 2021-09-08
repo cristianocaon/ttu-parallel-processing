8cf5667326ba518fc00db0b2e60756f668cff4eb
Cristiano Caon <cristiano.e.caon@gmail.com>
2021-09-08 15:27:09 -0500

    feat: add source code

--- Assignment 1/nblockingComm.c
+++ Assignment 1/nblockingComm.c
@@ -3,62 +3,61 @@
 #include <time.h>
 #include <mpi.h>
 
-#define generate_data(i,j)  (i)+(j)*(j)
+#define generate_data(i,j)
 
 int main( int argc, char **argv)
 {
   int i, j, pid, np, mtag, count;
-  double t0, t1, ;
-  int data[100][100], row_sum[100] ;
+  double t0, t1;
+  int data[100][100], row_sum[100];
   MPI_Status status;
   MPI_Request req_s, req_r;
 
-
-  MPI_Init( &argc, &argv );
+  MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &pid);
   MPI_Comm_size(MPI_COMM_WORLD, &np);
 
   if(pid == 0) { // generate data[]
     for(i=0; i<50; i++)
       for(j=0; j<100; j++)
-        data[i][j] = i+ j*j ;
-    mtag = 1 ;
-    MPI_Isend(data, 5000, MPI_INT, 1, mtag, MPI_COMM_WORLD, &req_s) ;
+        data[i][j] = i + j * j;
+    mtag = 1;
+
+    MPI_Isend(data, 5000, MPI_INT, 1, mtag, MPI_COMM_WORLD, &req_s);
+
     for(i=50; i<100; i++)
       for(j=0; j<100; j++)
-        data[i][j] = generate_data(i,j) ;
+        data[i][j] = i + j * j;
 
     for(i=50; i<100; i++) {
-      row_sum[i] = 0 ;
+      row_sum[i] = 0;
       for(j=0; j<100; j++)
-         row_sum[i] += data[i][j] ;
+         row_sum[i] += data[i][j];
     }
-    MPI_Wait(&req_s) ;
+
+    MPI_Wait(&req_s, &status);
 
    /*** receive computed row_sums from  pid 1 ***/
-    mtag = 2 ;
-    MPI_Recv(row_sum, 50, MPI_INT, 1, mtag, MPI_COMM_WORLD, &status) ;
+    mtag = 2;
+    MPI_Recv(row_sum, 50, MPI_INT, 1, mtag, MPI_COMM_WORLD, &status);
     for(i=0; i<100; i++) {
-       printf(" %d  ", row_sum[i]) ;
+       printf(" %d  ", row_sum[i]);
        if(i%5 ==0) printf("\n");
     }
-  }
-  else { /*** pid == 1 ***/
-    mtag = 1 ;
-    MPI_Recv(data, 5000, MPI_INT, 0, mtag, MPI_COMM_WORLD, &status) ;
+  } else { /*** pid == 1 ***/
+    mtag = 1;
+    MPI_Recv(data, 5000, MPI_INT, 0, mtag, MPI_COMM_WORLD, &status);
 
     for(i=0; i<50; i++) {
-      row_sum[i] = 0 ;
+      row_sum[i] = 0;
       for(j=0; j<100; j++)
-         row_sum[i] += data[i][j] ;
+         row_sum[i] += data[i][j];
     }
 
-
    /*** Send computed row_sums to pid 0 ***/
-    mtag = 2 ;
-    MPI_Send(row_sum, 50, MPI_INT, 0, mtag, MPI_COMM_WORLD) ;
+    mtag = 2;
+    MPI_Send(row_sum, 50, MPI_INT, 0, mtag, MPI_COMM_WORLD);
   }
-
 
   MPI_Finalize();
 
