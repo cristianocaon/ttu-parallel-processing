d85f5794f702cc19f80b0097a0d8d21d268e0192
Cristiano Caon <cristiano.caon@hotmail.com>
2021-11-18 20:46:59 -0600

    feat: initialize data and transfer to GPU

--- Assignment 6/main.cu
+++ Assignment 6/main.cu
@@ -14,49 +14,45 @@
 #include <math.h>
 #include <time.h>
 
+#define N 6
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 
-__global__ void findMinValues(int N, int *edge)
+__global__ void findMinValues(int *edge)
 {
   int i, j, k;
-  // int result[N][N];
-  // int vBuf[N], hBuf[N];
+  int *vBuf, *hBuf;
 
-  for (k = 0; k < N; k++)
-  {
-    printf("%d\n", edge[k]);
-    // for (i = 0; i < N; i++)
-    //   vBuf[i] = edge[i][k];
+  // for (k = 0; k < N; k++)
+  // {
+  // for (i = 0; i < N; i++)
+  //   vBuf[i] = edge[i][k];
 
-    // for (j = 0; j < N; j++)
-    //   hBuf[j] = edge[k][j];
+  // for (j = 0; j < N; j++)
+  //   hBuf[j] = edge[k][j];
 
-    // for (i = 0; i < N; i++)
-    //   for (j = 0; j < N; j++)
-    //     edge[i][j] = MIN(edge[i][j], (vBuf[i] + hBuf[j]));
-  }
+  // for (i = 0; i < N; i++)
+  //   for (j = 0; j < N; j++)
+  //     edge[i][j] = MIN(edge[i][j], (vBuf[i] + hBuf[j]));
+  // }
 }
 
-void main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-  int *edge, *cudaEdge, size, i, j, N = 6;
+  int *edge, *cudaEdge, size, i;
 
-  size = pow(N, 2) * sizeof(int);
+  size = sizeof(int) * pow(N, 2);
   edge = (int *)malloc(size);
 
-  for (i = 0; i < pow(N, 2); i++)
-  {
-    if (i % N == 0)
-      edge[i] = 0;
-
-    edge[i] = 2 * i;
-  }
+  for (i = 0; i < N; i++)
+    edge[i] = i % N == 0 ? 0 : i;
 
   cudaMalloc((void **)&cudaEdge, size);
   cudaMemcpy(cudaEdge, edge, size, cudaMemcpyHostToDevice);
 
-  findMinValues<<<1, 1>>>(N, edge);
+  findMinValues<<<1, 1>>>(cudaEdge);
 
-  free(edge);
   cudaFree(cudaEdge);
+  free(edge);
+
+  return 1;
 } /****************** End of function main() ********************/
